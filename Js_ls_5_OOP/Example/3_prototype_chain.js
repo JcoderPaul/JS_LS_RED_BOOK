133b18543348298a278c42ad1214ee527791b481
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-09-27 10:02:57 +0500

    Js_ls_5_OOP: add 3_prototype_chain.js

---
+++ Js_ls_5_OOP/Example/3_prototype_chain.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/* 
+Еще раз повторим:
+
+Шаг 1 - Класс с конструктором */
+class Car {
+        constructor(model, firm) {
+                this.model = model;
+                this.firm = firm;
+        }
+        
+        getModel(){
+                return this.model;
+        }
+
+        getFirm(){
+                return this.firm;
+        }
+}
+
+/* 
+Шаг 2 - Создание ссылки на будущий пустой объект
+Шаг 3 - Вызов конструктора 
+Шаг 4 - Свзывание с прототипом Car.prototype
+*/
+Car.prototype.color = 'green';
+
+const ladaPontarez = new Car();
+console.log(ladaPontarez); // Car { model: undefined, firm: undefined }
+console.log(Object.getPrototypeOf(ladaPontarez) === Car.prototype); // true
+
+ladaPontarez.speed = 360; // Добавляем свойство объекту ladaPontarez
+console.log(ladaPontarez); // Car { model: undefined, firm: undefined, speed: 360 }
+
+console.log(Object.hasOwn(ladaPontarez, 'speed')); // true - свойство объекта
+console.log(ladaPontarez.color); // green - свойство прототипа
+console.log(Object.hasOwn(ladaPontarez, 'color')); // false - свойство прототипа
+
+ladaPontarez.model = 'Pontorez';
+ladaPontarez.firm = 'VAZ';
+console.log(ladaPontarez); // Car { model: 'Pontorez', firm: 'VAZ' }
+
+console.log(Object.getPrototypeOf(ladaPontarez) === Car.prototype); // true
+

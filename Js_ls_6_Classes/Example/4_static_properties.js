dc77c36f607f59ae6da0262f3725570362407bab
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-10-01 07:05:16 +0500

    Js_ls_6_OOP: add 4_static_properties.js

---
+++ Js_ls_6_Classes/Example/4_static_properties.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const constNum = new Number(23); // Конструирование экземпляра класса 'в динамике'
+const maxSafeNum = Number.MAX_SAFE_INTEGER // Обращение к стартическому свойству
+
+console.log(constNum.toString()); //23
+console.log(maxSafeNum); //9007199254740991
+
+const myArr = new Array(23, 34, 22, -1); // Конструирование массива при помощи конструктора
+const myArrTwo = Array.from(myArr); // Обращение к стартическому свойству для создания массива
+
+console.log(myArr); // [ 23, 34, 22, -1 ]
+console.log(myArrTwo); // [ 23, 34, 22, -1 ]
+console.log(myArr === myArrTwo); // false - ссылки на разные объекты

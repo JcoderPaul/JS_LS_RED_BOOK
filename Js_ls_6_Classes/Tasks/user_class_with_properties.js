e138d92c69b18ee759fd3725ecfe53e4c7b8c881
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-10-01 07:06:00 +0500

    Js_ls_6_OOP: add user_class_with_properties.js

---
+++ Js_ls_6_Classes/Tasks/user_class_with_properties.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/*
+Реализовать класс пользователя, со свойствами:
+- логин;
+- пароль;
+
+Причем пароль при установке должен переворачиваться и в таком виде 
+храниться. Пароль и логин после создания 'напрямую' изменить нельзя.
+
+Добавить классу методы:
+- Смены паролья (передаем старый и новый пароль);
+- Сверки пароля;
+*/
+
+class User {
+  #login; // У него есть только геттер
+  #_pass; // У него есть и геттер и сеттер
+
+  constructor(login, password) {
+    this.#login = login;
+    /* Передаем пароль в 'шифрующий метод', обращаясь к тому, как к свойству, через '.' */
+    this.#setPass = password;
+  }
+
+  /* ...типа шифруем пароль */
+  set #setPass(password) {
+    this.#_pass = password.split('').reverse().join('');
+  }
+  /* ...типа дешифруем пароль и возвращаем */
+  get #getPass() {
+    return this.#_pass.split('').reverse().join('');
+  }
+
+  get getLogin() {
+    return this.#login;
+  }
+
+  /* Проверяем старый пароль перед сменой */
+  checkPassword(password) {
+    return this.#getPass === password;
+  }
+
+  changePass(oldPass, newPass) {
+    if (!this.checkPassword(oldPass)) {
+      return false;
+    } else {
+      /* Обращаемся к методу, как к свойству */
+      this.#setPass = newPass;
+      return true;
+    }
+  }
+}
+
+/* ----- Тест ----- */
+const userMalcolm = new User('m_stone@sw.de', '12345');
+
+console.log(userMalcolm); // User {}
+console.log(userMalcolm.getLogin); // m_stone@sw.de
+
+console.log(userMalcolm.checkPassword('12345')); // true
+console.log(userMalcolm.checkPassword('867655')); // false
+
+console.log(userMalcolm.changePass('12345', '09876')); // true
+console.log(userMalcolm.checkPassword('09876')); // true

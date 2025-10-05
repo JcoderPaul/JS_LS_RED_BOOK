921357bb56fa64669336223c839ec1a70e39ca68
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-10-05 07:23:55 +0500

    Js_ls_8: add O_pen_closed_principle.js

---
+++ Js_ls_8_SOLID/Example/O_pen_closed_principle.js
@@ -0,0 +1,76 @@
+'use strict';
+
+class Treasure {}
+
+class Coin extends Treasure {}
+
+class Crystal extends Treasure {}
+
+/* 
+Плохая реализация, т.к. работать оно будет, но в случае 
+появления нового 'инвентаря', нам придется переписывать
+реализацию класса (метода)
+*/
+class Inventary {
+  #score = 0;
+  pick(treasure) {
+    if (treasure instanceof Coin) {
+      this.#score += 1;
+    }
+    if (treasure instanceof Crystal) {
+      this.#score += 10;
+    }
+  }
+}
+
+/* 
+Новый инвентарь, но для того, чтобы учесть его в наборе 
+всего инвентаря персонажа, нужно переписать Inventary
+*/
+class Spice extends Treasure {
+
+}
+
+/* 
+Что-то типа: 
+
+class Inventary {
+  #score = 0;
+  pick(treasure) {
+    if (treasure instanceof Coin) {
+      this.#score += 1;
+    }
+    if (treasure instanceof Crystal) {
+      this.#score += 10;
+    }
+    if (treasure instanceof Spice) {
+      this.#score += 100;
+    }  
+  }
+}
+*/
+
+/* ----- Чуть более правильная реализация ----- */
+
+class Coolysh {
+  value = 0;
+}
+
+class Yurk extends Coolysh {
+  value = 1;
+}
+
+class Dattam extends Coolysh {
+  value = 10;
+}
+
+class Bacu extends Coolysh {
+  value = 100;
+}
+
+class BigBag {
+  #bag = 0;
+  pic(cool) {
+    this.#bag += cool.value;
+  }
+}

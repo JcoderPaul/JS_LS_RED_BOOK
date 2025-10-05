c51536922affc490dc7fd1f14b140218ce565e76
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-10-05 07:23:43 +0500

    Js_ls_8: add L_iskov_substitution_principle.js

---
+++ Js_ls_8_SOLID/Example/L_iskov_substitution_principle.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class User {
+  role = "user";
+
+  /* Тут мы возвращаем строку */
+  getRole() {
+    return this.role;
+  }
+}
+
+function logRole(user) {
+  console.log("Role: " + user.getRole().toUpperCase());
+}
+
+logRole(new User()); // Role: USER
+
+/* И тут у нас появился ADMIN */
+class Admin extends User{
+        role = ['user', 'admin']; 
+        /* Значит и тут в методе getRole() мы дожны возвращать строку*/
+}
+
+/* logRole(new Admin()); - TypeError: user.getRole(...).toUpperCase is not a function */
+
+/* И тогда либо через протопип */
+Admin.prototype.getRole = function() {
+    return this.role.join(',');
+}
+
+logRole(new Admin());
+
+/* 
+Либо переопредление метода: 
+
+class Admin extends User{
+        #role = ['user', 'admin']; 
+        
+        getRole() {
+                return this.role.join(',');
+        }
+*/

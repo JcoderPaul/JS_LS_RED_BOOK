853bba49cba4435e2e3c36dff3f93816b45fba4e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-10-16 07:36:01 +0500

    Js_ls_12: update 1_async_await_example.js

--- Js_ls_12_Async_await_modern_code/Example/1_async_await_example/1_async_await_example.js
+++ Js_ls_12_Async_await_modern_code/Example/1_async_await_example/1_async_await_example.js
@@ -11,11 +11,11 @@ getProductPrm(); // {products: Array(30), total: 194, skip: 0, limit: 30}
 console.log('End prm.'); // End prm.
 
 /* Асинхронная функция на async/await */
-async function getProductAsnc() {
+async function getProductAsync() {
         const resp = await fetch('https://dummyjson.com/products');
         const data = await resp.json();
         console.log(data);
 }
 
-getProductAsnc(); // {products: Array(30), total: 194, skip: 0, limit: 30}
-console.log('End asnc.'); // End asnc.
+getProductAsync(); // {products: Array(30), total: 194, skip: 0, limit: 30}
+console.log('End async.'); // End async.
